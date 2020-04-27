0599ba357a5506fad62e342b056461fbdd11b4ee
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-04-27 19:27:43 +0200

    add test for organization member

---
+++ packages/models/src/__test__/organization-member.spec.ts
@@ -0,0 +1,7 @@
+import { OrganizationMember } from "../organization-member";
+
+describe("Organization member", () => {
+  it("compiles", () => {
+    expect({} as OrganizationMember).toBeDefined();
+  });
+});
