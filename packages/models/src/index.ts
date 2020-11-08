68540d01b5869cedd15f353ce5fccb5d9e7b806d
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-09 00:20:13 +0100

    add fetcher props

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,3 +1,4 @@
 export { Organization } from "./organization/organization";
 export { OrganizationMember } from "./organization/organization-member";
 export { DataSource } from "./data-fetching/data-source";
+export { FetcherProps } from "./data-fetching/fetcher-props";
