3dafb9ffc30c3940d9e42b7d31eb68e6d6b3b6dd
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-08 22:10:40 +0100

    rename data source property

--- packages/models/src/data-fetching/data-source.ts
+++ packages/models/src/data-fetching/data-source.ts
@@ -1,9 +1,8 @@
 import { ApiConfiguration } from "./api-configuration";
 import { SqlConfiguration } from "./sql-configuration";
 
-//         { type: 'sql', handles: ['main-db'], cloudAccessible: true, connectionString: 'sample-conn-string' }
 export type DataSource = {
   handles: string[];
-  cloudAccessible: boolean;
+  directAccess: boolean;
   configuration: SqlConfiguration | ApiConfiguration;
 };
