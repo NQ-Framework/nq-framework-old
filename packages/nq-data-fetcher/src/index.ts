6e1e59d77550bbcb0a1e0210430245521fa8ed98
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-09 23:57:32 +0100

    fix typo

--- packages/nq-data-fetcher/src/index.ts
+++ packages/nq-data-fetcher/src/index.ts
@@ -1,3 +1,3 @@
 export { SqlFetcher } from "./sql/sql-fetcher";
 export { DataFetcherFactory } from "./dataFetcherFactory";
-export { DataFetcherInterface as DataFetcher } from "./dataFetcherInterface";
+export { DataFetcherInterface } from "./dataFetcherInterface";
