513c91b866e9d6d8b8be57e17dd3afcdd5a4182b
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-09 23:38:20 +0100

    rename package, add exports

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,3 +1,5 @@
 export { Organization } from "./organization/organization";
 export { OrganizationMember } from "./organization/organization-member";
 export { DataSource } from "./data-fetching/data-source";
+export { MsSqlConfiguration } from "./data-fetching/ms-sql-configuration";
+export { ApiConfiguration } from "./data-fetching/api-configuration";
