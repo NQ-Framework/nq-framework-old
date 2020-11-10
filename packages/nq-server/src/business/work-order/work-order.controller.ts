00a79a50f43b27f49700baecc489161f450aacd2
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-10 15:25:21 +0100

    add flexible test method to run any sql
    this needs to be removed before hitting actual production

--- packages/nq-server/src/business/work-order/work-order.controller.ts
+++ packages/nq-server/src/business/work-order/work-order.controller.ts
@@ -2,10 +2,11 @@ import { Controller, Get, Query, Req } from '@nestjs/common';
 import { auth } from 'firebase-admin';
 import { RequestRouterService } from '../../db-connection/request-router/request-router.service';
 import { MsSqlFetcher } from '@nq-framework/data-fetcher';
+import { TYPES } from 'tedious';
 
 @Controller('work-order')
 export class WorkOrderController {
-  constructor(private router: RequestRouterService) {}
+  constructor(private router: RequestRouterService) { }
 
   @Get('')
   async documents(
@@ -20,8 +21,39 @@ export class WorkOrderController {
       dataSource,
     )) as MsSqlFetcher;
     const data = await response.get({
-      isProcedure: false,
-      query: 'select * from tblUser_Info',
+      isProcedure: true,
+      query: 'testProc',
+      params: [
+        { name: 'TestParam', type: TYPES.Int, value: 10 }
+      ],
+      outParams: [
+        { name: "TestOut", type: TYPES.Int }
+      ]
+    });
+    return { data };
+  }
+
+  @Get('anything')
+  async getAnything(
+    @Req() req: any,
+    @Query('organizationId') organizationId: string,
+    @Query('dataSource') dataSource: string,
+    @Query('isProcedure') isProcedure: string,
+    @Query('query') query: string
+  ): Promise<{ data: any }> {
+    const user = req.firebaseUser as auth.DecodedIdToken;
+    const response = (await this.router.getDataFetcher(
+      user.uid,
+      organizationId,
+      dataSource,
+    )) as MsSqlFetcher;
+    const data = await response.get({
+      isProcedure: isProcedure === "yes" || isProcedure === "true",
+      query,
+      params: [
+      ],
+      outParams: [
+      ]
     });
     return { data };
   }
