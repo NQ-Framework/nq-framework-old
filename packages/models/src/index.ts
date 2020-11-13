1410d8e515d83e3b66fe75701b71bb3c18e4a176
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-13 17:57:05 +0100

    export triggered job module in index

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -4,6 +4,7 @@ export { DataSource } from "./data-fetching/data-source";
 export { MsSqlConfiguration } from "./data-fetching/ms-sql-configuration";
 export { ApiConfiguration } from "./data-fetching/api-configuration";
 export { ScheduledJob } from "./scheduled-jobs/scheduled-job";
+export { TriggeredJob } from "./triggered-jobs/triggered-job";
 export { ConfigurationInterface } from "./jobs/configuration-interface";
 export { LogConfiguration } from "./jobs/log-configuration";
 export {
