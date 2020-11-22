999bc25f6203544a90e8efa94a1f1417375caaeb
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-22 14:33:35 +0100

    fix tests

--- packages/server/src/workflow/workflow.controller.spec.ts
+++ packages/server/src/workflow/workflow.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorkflowController } from './workflow.controller';
 import { WorkflowRepositoryService } from './workflow-repository.service';
+import { ActionsRepositoryService } from '../actions/actions-repository/actions-repository.service';
 
 describe('WorkflowController', () => {
   let controller: WorkflowController;
@@ -8,7 +9,7 @@ describe('WorkflowController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WorkflowController],
-      providers: [{ provide: WorkflowRepositoryService, useValue: {} }],
+      providers: [{ provide: WorkflowRepositoryService, useValue: {} }, { provide: ActionsRepositoryService, useValue: {} }],
     }).compile();
 
     controller = module.get<WorkflowController>(WorkflowController);
