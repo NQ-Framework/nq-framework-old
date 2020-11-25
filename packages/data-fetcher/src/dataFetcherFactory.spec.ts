33ac4841e1c1dabbf23643b5e7f6255cd65ee06c
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-25 16:20:29 +0100

    fix unit tests for fetcher

--- packages/data-fetcher/src/dataFetcherFactory.spec.ts
+++ packages/data-fetcher/src/dataFetcherFactory.spec.ts
@@ -1,37 +1,42 @@
-import { MsSqlConfiguration } from "@nqframework/models";
+import { DataCredentials, reducePropertyValuesToObject } from "@nqframework/models";
 import { DataFetcherFactory } from "./dataFetcherFactory"
 import { MsSqlFetcher } from "./sql/ms-sql-fetcher"
 jest.mock('./sql/ms-sql-fetcher');
-const mockConnectionConfiguration: MsSqlConfiguration = {
-    type: 'mssql',
-    username: 'username',
-    password: 'password',
-    serverIp: 'localhost',
-    port: '1234',
-    database: 'database',
-    trustServerCertificate: true
+const mockCredentials: DataCredentials = {
+    name: 'mock credentials',
+    isRemote: false,
+    configuration: [
+        { name: 'serverIp', value: "test" },
+        { name: 'port', value: "test" },
+        { name: 'username', value: "test" },
+        { name: 'password', value: "test" },
+        { name: 'database', value: "test" },
+        { name: 'type', value: "mssql" },
+        { name: 'trustServerCertificate', value: true },
+    ],
+    credentialsType: {
+        description: 'mock desc',
+        isEnabled: true,
+        name: 'mock credentials type',
+        properties: [],
+        type: 'mssql'
+    }
 }
 describe('DataFetcherFactory', () => {
     it('should be defined', () => {
         expect(new DataFetcherFactory()).toBeDefined();
     });
     it('should return sql fetcher if config type is sql', () => {
-        const fetcher = new DataFetcherFactory().create({
-            configuration: mockConnectionConfiguration,
-        } as any)
+        const fetcher = new DataFetcherFactory().create("mssql", mockCredentials.configuration)
         expect(fetcher).toBeDefined();
         const mockFetcher = MsSqlFetcher as jest.Mock;
         expect(mockFetcher.mock.instances.length).toEqual(1);
-        expect(mockFetcher).toHaveBeenCalledWith(mockConnectionConfiguration);
+        expect(mockFetcher).toHaveBeenCalledWith(reducePropertyValuesToObject(mockCredentials.configuration));
     });
 
     it('should throw for invalid configurations', () => {
         expect(() => {
-            new DataFetcherFactory().create({
-                configuration: {
-                    type: 'non-sql',
-                },
-            } as any)
+            new DataFetcherFactory().create("invalid", [])
         }).toThrowErrorMatchingSnapshot();
     });
 });
