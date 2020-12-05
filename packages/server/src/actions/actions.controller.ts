1e79a16c2b3c11f353eb9e9d17b527c4e1c2c5a3
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-12-05 16:08:02 +0100

    remove legacy trigger read from actions service

--- packages/server/src/actions/actions.controller.ts
+++ packages/server/src/actions/actions.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { Action, WorkflowTrigger } from '@nqframework/models';
+import { Action } from '@nqframework/models';
 import { ActionsRepositoryService } from './actions-repository/actions-repository.service';
 
 @Controller('actions')
@@ -9,10 +9,5 @@ export class ActionsController {
   @Get('')
   async getEnabledActions(): Promise<Action[]> {
     return await this.actions.getEnabledActions();
-  }
-
-  @Get('triggers')
-  async getEnabledTriggers(): Promise<WorkflowTrigger[]> {
-    return await this.actions.getEnabledTriggers();
   }
 }
